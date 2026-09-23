869a632af9db55ab92e0cc058db61c8d305b2286
Azanth S <sazanth75@gmail.com>
2026-09-23 15:57:04 +0530

    Update XML sitemap with XSL stylesheet styling and updated lastmod timestamps

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -132,6 +132,7 @@ async function generateSitemap() {
   });
 
   const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${urls.join('')}
 </urlset>`;
